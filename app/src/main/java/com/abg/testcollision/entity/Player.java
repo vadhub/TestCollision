5a205ed5939a9504af7294aea60666b5c2f5f667
vadim <gabderahmanov99@gmail.com>
2024-10-24 19:06:15 +0300

    damage player

--- app/src/main/java/com/abg/testcollision/entity/Player.java
+++ app/src/main/java/com/abg/testcollision/entity/Player.java
@@ -8,13 +8,23 @@ public class Player extends GameObject{
     //спрайт
     public Bitmap bmp;
     public Sprite sprite;
+    public int hp;
 
     //конструктор
-    public Player(int x, int y, Bitmap bmp) {
+    public Player(int x, int y, Bitmap bmp, int hp) {
         this.bmp = bmp;                    //возвращаем рисунок
         this.x = x;                        //отступ по х нет
         this.y = y; //делаем по центру
         sprite = new Sprite(bmp, x, y,4);
+        this.hp = hp;
+    }
+
+    public void damage(Destroy destroy) {
+        if (hp > 0) {
+            hp--;
+        } else {
+            destroy.onDestroy();
+        }
     }
 
     //рисуем наш спрайт
