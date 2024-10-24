a62bb477460c57060ee781554f684f204a94d926
vadim <gabderahmanov99@gmail.com>
2024-10-24 19:05:51 +0300

    state damage wall

--- app/src/main/java/com/abg/testcollision/entity/Wall.java
+++ app/src/main/java/com/abg/testcollision/entity/Wall.java
@@ -1,34 +1,51 @@
 package com.abg.testcollision.entity;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 
+import com.abg.testcollision.R;
 import com.abg.testcollision.gamemode.GameMode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Wall extends GameObject {
-    GameMode gameView;
 
     //спрайт
-    public Bitmap bmp;
+    public List<Bitmap> bmp = new ArrayList<>();
 
-    public int hp = 3;
+    public int hp = 2;
 
     //конструктор
-    public Wall(GameMode gameView, Bitmap bmp, int x, int y) {
-        this.gameView = gameView;
-        this.bmp = bmp;
-        this.x = x;
-        this.y = y;
-        this.width = 32;
-        this.height = 64;
+    public Wall(Resources res, int x, int y) {
+        bmp.add(BitmapFactory.decodeResource(res, R.drawable.wall_0));
+        bmp.add(BitmapFactory.decodeResource(res, R.drawable.wall_1));
+        bmp.add(BitmapFactory.decodeResource(res, R.drawable.wall_2));
+        this.x = x + 10;
+        this.y = y + 10;
+        this.width = bmp.get(0).getWidth() + 10;
+        this.height = bmp.get(0).getHeight() + 10;
     }
 
     //рисуем наш спрайт
     public void onDraw(Canvas c) {
-        c.drawBitmap(bmp, x, y, null);
+        if (hp == 2) {
+            c.drawBitmap(bmp.get(0), x, y, null);
+        } else if (hp == 1) {
+            c.drawBitmap(bmp.get(1), x, y, null);
+        }  else if (hp == 0) {
+            c.drawBitmap(bmp.get(2), x, y, null);
+        }
+
     }
 
-    public void destroyWall() {
-        hp--;
+    public void damage(Destroy destroy) {
+        if (hp > 0) {
+            hp--;
+        } else {
+            destroy.onDestroy();
+        }
     }
 }
